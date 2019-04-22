246c5363fcfacbdefb9cacd07b17095663db3033
Usuario <Usuario@A14-M05>
2019-04-22 12:40:12 -0300

    Primer ingreso

---
+++ JSLogin/web/programa.js
@@ -0,0 +1,18 @@
+class Cuenta {
+    
+    
+    
+    static ingresar(){
+        console.log("Estamos dentro del metodo ingresar ahora.");
+        
+        console.log("El usuario ingresado es: " + document.querySelector("#usuario").value);
+        console.log("La clave ingresada es: " + document.querySelector("#clave").value);
+    }
+    
+    static registro(){
+        
+    }
+        
+    }
+        
+    
